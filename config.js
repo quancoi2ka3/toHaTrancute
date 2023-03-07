ec0e3add2f62c03852668abee538bb742e964117
quancoi2ka3 <106460115+quancoi2ka3@users.noreply.github.com>
2023-03-07 16:45:30 +0700

    Add files via upload
    creategift

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "Hà Ngố";
+const giftUrl = "https://www.facebook.com/keobongngotngao234";
+const eventName = "Chúc Mừng 8-3";
+const titleCard = "Gửi Tới keobongngotngao";
+const contentCard =
+  "8-3 Chúc đằng ấy luôn luôn xinh đẹp, ăn ngon ngủ tốt, biết yêu thương bản thân hơn và luôn nở nụ cười cho nó cute nhá :D(ảnh này thấy Hà cưng voãi hehe :'>)";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "girlHT_.png";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
